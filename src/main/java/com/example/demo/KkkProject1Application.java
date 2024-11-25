5fd950ae7783d12ee1ac1a8ced6d6dff55ca7d55
Raymond <raycha77@gmail.com>
2024-11-25 10:55:10 +0900

    Project created

---
+++ src/main/java/com/example/demo/KkkProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkkProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkkProject1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	public void home() {
+		System.out.println("home");
+	}
+}
